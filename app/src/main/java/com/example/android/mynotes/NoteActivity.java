a9044b727eac3cc9c1dd38df6a81af612705566a
SasTomasz <boss7@op.pl>
2019-05-16 05:47:58 +0200

    add skeleton of edit and view mode

--- app/src/main/java/com/example/android/mynotes/NoteActivity.java
+++ app/src/main/java/com/example/android/mynotes/NoteActivity.java
@@ -3,21 +3,49 @@ package com.example.android.mynotes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.android.mynotes.models.Note;
 
 public class NoteActivity extends AppCompatActivity {
     private static final String TAG = "NoteActivity";
+    // ui components
+    LinedEditText mLinedEditText;
+    EditText mToolbarEditText;
+    TextView mToolbarTextView;
+
+    // vars
+    boolean mIsThisNewNote;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        mLinedEditText = findViewById(R.id.et_note_activity);
+        mToolbarEditText = findViewById(R.id.et_toolbar);
+        mToolbarTextView = findViewById(R.id.tv_toolbar);
 
+        receiveIntent();
+        if (receiveIntent()){
+            // edit mode
+        } else {
+            // view mode
+        }
+
+    }
+
+    public boolean receiveIntent(){
         // check the activity has an extra
         if (getIntent().hasExtra("selected_note")){
             Note note = getIntent().getParcelableExtra("selected_note");
             Log.d(TAG, "onCreate: " + note.toString());
+            mIsThisNewNote = false;
+            return mIsThisNewNote;
         }
+        mIsThisNewNote = true;
+        return mIsThisNewNote;
     }
 }
