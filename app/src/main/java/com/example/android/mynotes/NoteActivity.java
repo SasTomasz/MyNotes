a4d8410a1c4a2daf77705fcbfa13c1f870f95645
SasTomasz <boss7@op.pl>
2019-06-20 08:34:12 +0200

    handling with rotation changes

--- app/src/main/java/com/example/android/mynotes/NoteActivity.java
+++ app/src/main/java/com/example/android/mynotes/NoteActivity.java
@@ -246,4 +246,19 @@ public class NoteActivity extends AppCompatActivity
         }
 
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("mode", mMode);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mMode = savedInstanceState.getInt("mode");
+        if (mMode == ENABLED_EDIT_MODE){
+            enableEditMode();
+        }
+    }
 }
