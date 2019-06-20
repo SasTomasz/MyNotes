b392df62933c85eadbf8b80313be28e211b5c710
SasTomasz <boss7@op.pl>
2019-06-20 08:03:25 +0200

    add swiping in list of notes to delete particular note

--- app/src/main/java/com/example/android/mynotes/MainActivity.java
+++ app/src/main/java/com/example/android/mynotes/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.android.mynotes;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
         RecyclerDecoration recyclerDecoration = new RecyclerDecoration(8);
+        new ItemTouchHelper(itemTouchHelper).attachToRecyclerView(mRecyclerView);
         mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes, this);
         mRecyclerView.addItemDecoration(recyclerDecoration);
         mRecyclerView.setAdapter(mNotesRecyclerAdapter);
@@ -78,4 +81,22 @@ public class MainActivity extends AppCompatActivity
         Intent intent = new Intent(this, NoteActivity.class);
         startActivity(intent);
     }
+
+    private void deleteNote(Note note){
+        mNotes.remove(note);
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    // add delete by swiping feature
+    private ItemTouchHelper.SimpleCallback itemTouchHelper = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            deleteNote(mNotes.get(viewHolder.getAdapterPosition()));
+        }
+    };
 }
